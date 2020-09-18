4a75f667c31229821ef5d417cdfe6bbc1127c99f
Krishna PrabhuGaonkar <krishna.prabhugaonkar@indegene.com>
2020-09-18 12:53:33 +0530

    Updates

---
+++ precache-manifest.e576e455eded0d78338d8c3386e0abe7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6851ddc112bc6b1eb3e56cc100adbe33",
+    "url": "/Simple-Todo-App---React-Hooks/index.html"
+  },
+  {
+    "revision": "747515297a5225e35c6f",
+    "url": "/Simple-Todo-App---React-Hooks/static/css/main.4340803f.chunk.css"
+  },
+  {
+    "revision": "ab5f661e6244cabdbe6a",
+    "url": "/Simple-Todo-App---React-Hooks/static/js/2.22288252.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Simple-Todo-App---React-Hooks/static/js/2.22288252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "747515297a5225e35c6f",
+    "url": "/Simple-Todo-App---React-Hooks/static/js/main.d0f7927a.chunk.js"
+  },
+  {
+    "revision": "cfa706bb85d88e776635",
+    "url": "/Simple-Todo-App---React-Hooks/static/js/runtime-main.d8f54b7a.js"
+  }
+]);
